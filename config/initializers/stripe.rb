eea33a79e2e3ef3e3767fe878a6314af0b5655db
yuyakitano <yuyafitness88@gmail.com>
2021-09-19 13:56:58 +0900

    面接前最終

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,4 +1,10 @@
-Stripe.api_key = Rails.application.credentials.stripe_api_key
+Rails.configuration.stripe = {
+  publishable_key: Rails.application.secrets.stripe_publishable_key,
+  secret_key: Rails.application.secrets.stripe_secret_key
+}
+
+Stripe.api_key = Rails.application.secrets.stripe_secret_key
+# Stripe.api_key = Rails.application.credentials.stripe_api_key
 # Stripe.api_version = "2021-09-17"
 
 #各々のキーは
